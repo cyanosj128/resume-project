9423522027150885bd10a4bdbd3c15dbee94e00e
SeungJooNoh <cyanosj128@gmail.com>
2024-09-30 10:46:18 +0900

    apply navigation components with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/Home';
+import DexPage from './pages/Dex';
+import MapPage from './pages/Map';
+import NavigationBar from './components/NavigationBar';
+import NavigationButton from './components/NavigationButton';
 
 const routes = [
-  { key: 'home', label: 'Home', path: '/', element: <div>home</div> },
-  { key: 'map', label: 'Map', path: '/map', element: <div>map</div> },
-  { key: 'dex', label: 'DEX', path: '/dex', element: <div>dex</div> },
-  { key: 'cms', label: 'CMS', path: '/cms', element: <div>cms</div> },
+  { key: 'home', label: 'Home', path: '/', element: <HomePage /> },
+  { key: 'map', label: 'Map', path: '/map', element: <MapPage /> },
+  { key: 'dex', label: 'DEX', path: '/dex', element: <DexPage /> },
 ];
 
 function App() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      {routes.map((route) => (
-        <button
-          key={`${route.key}-button`}
-          onClick={() => navigate(route.path)}
-        >
-          {route.label}
-        </button>
-      ))}
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        padding: '24px',
+        backgroundColor: '#4c4c4c',
+      }}
+    >
+      <NavigationBar>
+        {routes.map((route) => (
+          <NavigationButton
+            key={`${route.key}-button`}
+            onClick={() => navigate(route.path)}
+            label={route.label}
+          />
+        ))}
+      </NavigationBar>
       <Routes>
         {routes.map((route) => (
           <Route key={route.key} path={route.path} element={route.element} />
