9e4c4f22c520cab081a98de258b12ad1e4caac08
SeungJooNoh <cyanosj128@gmail.com>
2024-09-30 10:03:54 +0900

    implement routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
+const routes = [
+  { key: 'home', label: 'Home', path: '/', element: <div>home</div> },
+  { key: 'map', label: 'Map', path: '/map', element: <div>map</div> },
+  { key: 'dex', label: 'DEX', path: '/dex', element: <div>dex</div> },
+  { key: 'cms', label: 'CMS', path: '/cms', element: <div>cms</div> },
+];
+
 function App() {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      {routes.map((route) => (
+        <button
+          key={`${route.key}-button`}
+          onClick={() => navigate(route.path)}
+        >
+          {route.label}
+        </button>
+      ))}
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.key} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
